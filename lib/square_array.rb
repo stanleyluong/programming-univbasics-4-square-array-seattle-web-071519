604c87edec2a23bb9ccfae68279d9818d20986b8
crisp-convention-4859 <slluong@uw.edu>
2019-07-05 16:34:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,8 @@
-
-
-
 def square_array(array)
-  counter = 0
-  while counter <= array.length do
-    puts array[counter] ** 2
-    counter += 1
+  i = 0
+  while i < array.length do
+    p (array[i] ** 2)
+    i += 1
+  end 
 end
+
